d577484246191d603f487aa53ac19214f87983cb
Lee-Sang-Beom <tkdqja975@naver.com>
2022-12-02 22:32:05 +0900

    add content : undefined, undeclared, nan, null

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log(this)
+const obj = { a: 1, b: 2, c: 3 }
+const {c, ...rest} = obj;
+console.log(rest);
+
+const arr = ["a", "b", "c", "d"]
+const [firstElement, ...rest2] = arr
+console.log(firstElement, arr) // "a"
+console.log(rest2) // ["b", "c", "d"]
